f2a4891b0c62a4cc8e89aceea7e012d47ca356b8
Adam Codega <acodega@users.noreply.github.com>
2022-02-17 12:56:15 -0500

    Added logic to find Wi-Fi interface and disconnect

--- removeSSID.sh
+++ removeSSID.sh
@@ -3,8 +3,10 @@
 #
 # Check if an SSID exists and delete it if it does.
 # Change the wifinetwork variable to the name of your SSID.
+# Turn Wi-Fi off and on in order to disconnect from the removed network.
 #
 
+wifiInterface=$(/usr/sbin/networksetup -listallhardwareports | /usr/bin/awk '/Wi-Fi|AirPort/ {getline; print $NF}')
 wifiNetwork="Contoso Inc"
 searchResult=$(networksetup -listpreferredwirelessnetworks en0 | grep "$wifiNetwork")
 
@@ -17,6 +19,8 @@ else
 fi
 
 # Delete it
-networksetup -removepreferredwirelessnetwork en0 "$wifiNetwork"
-echo "$wifiNetwork SSID Removed"
-exit 0
+networksetup -removepreferredwirelessnetwork $wifiInterface "$wifiNetwork"
+
+networksetup -setairportpower $wifiInterface off
+sleep 0.5
+networksetup -setairportpower $wifiInterface on
