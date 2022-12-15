246f99a03cb4d4525006c2f17cb31d1d2e2d3118
Adam Codega <acodega@users.noreply.github.com>
2022-12-15 17:25:05 -0500

    Update prepDockLauncher.sh

--- prepDockLauncher.sh
+++ prepDockLauncher.sh
@@ -33,6 +33,8 @@ autoload is-at-least
 installedOSversion=\$(sw_vers -productVersion)
 launchdaemon_filepath="/Library/LaunchDaemons/com.contoso.docksettings.plist"
 currentUser=\$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print \$3 }' )
+uid=\$(id -u "\$currentUser")
+echo "\$currentUser and \$uid"
 
 if is-at-least 13 "\$installedOSversion"; then
   settingsPath=/System/Applications/System\ Settings.app
@@ -42,7 +44,7 @@ fi
 
 runAsUser() {  
   if [ "\$currentUser" != "loginwindow" ]; then
-    launchctl asuser "\$uid" sudo -u "\$currentUser" "$@"
+    launchctl asuser "\$uid" sudo -u "\$currentUser" "\$@"
   else
     echo "no user logged in"
   fi
@@ -52,22 +54,25 @@ dock_item() {
   printf '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>%s</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>', "\$1"
 }
 
+sleep 5
+
+runAsUser defaults write com.apple.terminal SecureKeyboardEntry -bool "true"
 runAsUser defaults delete com.apple.dock persistent-apps
-runAsUser defaults delete com.apple.dock recent-apps
+runAsUser defaults write com.apple.dock "show-recents" -bool "false"
 
 runAsUser defaults write com.apple.dock persistent-apps -array \
-  "\$(dock_item /Applications/Launchpad.app)" \
+  "\$(dock_item /System/Applications/Launchpad.app)" \
   "\$(dock_item /Applications/Google\ Chrome.app)" \
   "\$(dock_item /Applications/Slack.app)" \
   "\$(dock_item /Applications/zoom.us.app)" \
+  "\$(dock_item /Applications/Self Service.app)" \
   "\$(dock_item "\$settingsPath")"
 
-/usr/local/bin/kandji dock --add io.kandji.Self-Service
+killall Dock
 
 launchctl unload "\${launchdaemon_filepath}"
 rm "\${launchdaemon_filepath}"
-rm "\${launchdaemon_program_filepath}"
-
+rm "/tmp/setDock.sh"
 
 exit 0
 
