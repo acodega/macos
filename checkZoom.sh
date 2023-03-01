83e30c38e8d632232cd65588de3b6cd641b87768
Adam Codega <acodega@users.noreply.github.com>
2023-03-01 17:24:59 -0500

    Create checkZoom.sh

---
+++ checkZoom.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+if [ -e /Applications/zoom.us.app ]; then
+    zoomStatus=$(/usr/libexec/PlistBuddy -c "print ZITPackage" /Applications/zoom.us.app/Contents/Info.plist 2>/dev/null)
+else echo "Zoom not installed"
+exit 0
+fi
+
+if [[ $zoomStatus == true ]]; then
+    zoomAppType="zoomforIT"
+else zoomAppType="consumer"
+fi
+
+echo "Zoom installed is $zoomAppType"
