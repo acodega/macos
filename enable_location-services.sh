63fbf57c5d49aadbb4ec657be6ee55282e433e9c
Adam Codega <acodega@users.noreply.github.com>
2025-02-04 10:41:33 -0500

    Update enable_location-services.sh
    Sequoia compatibility/expanded comments

--- enable_location-services.sh
+++ enable_location-services.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-# Imaging? Need to turn on Location Services programmatically?
-# Deploy this.
+# Turn on Location Services programmatically
 #
 # People travel. Don't set the time zone manually.
 #
 # Adam Codega
 # github.com/acodega
 #
+# Tested on macOS Sequoia 15.1.1 and earlier
+#
+# pre-SIP commands have been left for historical/education reasons
+# Mac has to restart after running, Location Services will be enabled then
+#
 
 # unload locationd for pre-SIP systems, comment this out if SIP
-/bin/launchctl unload /System/Library/LaunchDaemons/com.apple.locationd.plist
+# /bin/launchctl unload /System/Library/LaunchDaemons/com.apple.locationd.plist
 
 # write enabled value to locationd plist
 /usr/bin/defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd LocationServicesEnabled -int 1
 
 # fix ownership of the locationd folder, just to be safe, comment this out if SIP
-/usr/sbin/chown -R _locationd:_locationd /var/db/locationd
+# /usr/sbin/chown -R _locationd:_locationd /var/db/locationd
 
 # reload locationd for pre-SIP systems
-/bin/launchctl load /System/Library/LaunchDaemons/com.apple.locationd.plist
+# /bin/launchctl load /System/Library/LaunchDaemons/com.apple.locationd.plist
